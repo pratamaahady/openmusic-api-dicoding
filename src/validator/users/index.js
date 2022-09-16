a8849e0cd03df09db8946c19b050ab8b8c7519a1
Hady Pratama <pratamaahady@gmail.com>
2022-09-16 13:53:20 +0700

    fix: eslint style

--- src/validator/users/index.js
+++ src/validator/users/index.js
@@ -1,6 +1,6 @@
 const InvariantError = require('../../exceptions/InvariantError');
 const { UserPayloadSchema } = require('./schema');
- 
+
 const UsersValidator = {
   validateUserPayload: (payload) => {
     const validationResult = UserPayloadSchema.validate(payload);
@@ -9,5 +9,5 @@ const UsersValidator = {
     }
   },
 };
- 
+
 module.exports = UsersValidator;
